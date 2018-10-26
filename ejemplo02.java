0a1ec9cc8e72bc6e85417bef0dee7088a7fe96b0
Lizccr <44419774+Lizccr@users.noreply.github.com>
2018-10-26 13:12:30 -0500

    Create ejemplo02.java

---
+++ ejemplo02.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Ejemplo04{
+  public static void main(String[]ARGS){
+   Scanner sc = new Scanner (System.in);
+   int num;
+   System.out.println("Introduzca numero: ");
+   num = sc.nestInt();
+   if((num%2)=00)
+     System.out.prinln("PAR");
+   else
+     System.out.prinln("IMPAR");
+ }
+}
